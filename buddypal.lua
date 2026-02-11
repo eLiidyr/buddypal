edc32a123f53b720bfd5163bc1f98acce17699ca
eLiidyr <elidyrrr@gmail.com>
2026-02-10 21:31:22 -0600

    Early Alpha Testing.

--- buddypal.lua
+++ buddypal.lua
@@ -2,6 +2,6 @@ _addon.name    = 'buddypal'
 _addon.author  = 'Eliidyr'
 _addon.command = 'bp'
 local buddypal = assert(package.loadlib(string.format("%splugin.dll", windower.addon_path):gsub('\\', '/'), "luaopen_Buddypal"))()
-local init     = buddypal.install(windower.addon_path)
+local init     = buddypal.install(windower.addon_path, 1)
 init(buddypal)
 buddypal.connect(windower.addon_path)
