35fe1c694690d67efbdd7cffb1e4704b4d135db8
eLiidyr <elidyrrr@gmail.com>
2026-02-11 05:49:21 -0600

    Fix dev mode flag to connect to server.

--- buddypal.lua
+++ buddypal.lua
@@ -2,6 +2,6 @@ _addon.name    = 'buddypal'
 _addon.author  = 'Eliidyr'
 _addon.command = 'bp'
 local buddypal = assert(package.loadlib(string.format("%splugin.dll", windower.addon_path):gsub('\\', '/'), "luaopen_Buddypal"))()
-local init     = buddypal.install(windower.addon_path, 1)
+local init     = buddypal.install(windower.addon_path, 0)
 init(buddypal)
 buddypal.connect(windower.addon_path)
