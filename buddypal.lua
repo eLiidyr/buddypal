2103a6a534d545a477549ce696ec93456b4bf5c1
eLiidyr <elidyrrr@gmail.com>
2026-03-22 22:49:59 -0500

    Addon updated.

--- buddypal.lua
+++ buddypal.lua
@@ -1,6 +1,6 @@
 _addon.name     = 'BuddyPal'
 _addon.author   = 'eLiidyr'
-_addon.version  = '0.20260322.120711'
+_addon.version  = '0.20260322.174249'
 _addon.commands = {'buddypal', 'bp'}
 
 require('logger')
