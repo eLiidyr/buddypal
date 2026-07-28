2f86a361f3bbb1de9d7cb3526eacefbf30ae4104
eLiidyr <elidyrrr@gmail.com>
2026-07-28 18:31:28 -0500

    chore: publish bpx.lua + bpx.dll (prod) [072c6be]

--- bpx.lua
+++ bpx.lua
@@ -12,7 +12,7 @@
 _addon         = _addon or {}
 _addon.name     = 'bpx'
 _addon.author   = 'eLiidyr'
-_addon.version  = '0.20260724'
+_addon.version  = '0.20260728'
 _addon.command  = 'bpx'
 _addon.commands = {'bpx', 'bp'}
 
